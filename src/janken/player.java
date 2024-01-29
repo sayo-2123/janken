261e8187c64742c42a3d67d0705552fa2777beda
sayo-2123 <classa1990@gmail.com>
2024-01-29 22:21:37 +0900

    playerクラスの作成

--- src/janken/player.java
+++ src/janken/player.java
@@ -1,5 +1,34 @@
 package janken;
 
 public class player {
+	public static final int STONE = 0;
+	public static final int SCRISSORS = 1;
+	public static final int PAPER = 2;
 
-}
+	private String name;
+	private int winCount = 0;
+
+	public int showHand() {
+		double randomNum = 0;
+		int playerHand = 0;
+		randomNum = Math.random() * 3;
+		if (randomNum < 1) {
+			playerHand = STONE;
+		} else if (randomNum < 2) {
+			playerHand = SCRISSORS;
+		} else {
+			playerHand = PAPER;
+		}
+		return playerHand;
+	};
+
+	public void notifyResult(boolean result) {
+		if (true == result) {
+			winCount += 1;
+		}
+	};
+
+	public int getWinCount() {
+		return winCount;
+	}
+};
